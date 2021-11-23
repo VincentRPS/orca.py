16f8cc42f1def2652f81b254e197d60eba4e5b95
Vincent <vincentbusiness55@gmail.com>
2021-11-22 19:40:03 -0800

    Remove

--- setup.py
+++ setup.py
@@ -9,20 +9,6 @@ with open("requirements.txt") as f:
 with open("README.md") as f:
     readme = f.read()
 
-extras_require = {
-    "voice": ["telecom-py==0.0.4"],
-    "http": ["flask==0.12.2"],
-    "yaml": ["pyyaml==3.12"],
-    "music": ["youtube_dl==2018.1.21"],
-    "performance": [
-        "erlpack==0.3.2" if sys.version_info.major == 2 else "earl-etf==2.1.2",
-        "ujson==1.35",
-        "wsaccel==0.6.2",
-    ],
-    "sharding": ["gipc==0.6.0"],
-    "docs": ["biblio==0.0.4"],
-}
-
 setup(
     name="orca.py",
     author="b1nzy",
@@ -35,7 +21,6 @@ setup(
     long_description_content_type="text/markdown",
     include_package_data=True,
     install_requires=requirements,
-    extras_require=extras_require,
     test_suite="tests",
     setup_requires=["pytest-runner==2.11.1"],
     tests_require=[
